e14b53c564315a9dcdba62ca2ec8c59cf4cf07cf
dcassel <daniel.cassel@sickkids.ca>
2014-06-24 12:18:20 -0400

    Changed Test Data for CIVET/anat Tests Bug Fixes: - in pMincApply REDUCE == TRUE - When parsing formulas, make a case for interaction terms to prevent warnings - Lowered Tolerance for CIVET Tests (rounding error) - Set testFunc as global for mincApply tests - Added column names for VertexAnova output

--- inst/tests/test_civet.r
+++ inst/tests/test_civet.r
@@ -1,7 +1,7 @@
 context("civet.readAllCivetFiles")
 
-gf = read.csv("/tmp/rminctestdata/POND-imaging.csv")
-gf = civet.getAllFilenames(gf,"POND.ID","POND","/tmp/rminctestdata/CIVET","TRUE","1.1.12")
+gf = read.csv("/tmp/rminctestdata/CIVET_TEST.csv")
+gf = civet.getAllFilenames(gf,"ID","POND","/tmp/rminctestdata/CIVET","TRUE","1.1.12")
 gf = civet.readAllCivetFiles("/tmp/rminctestdata/AAL.csv",gf)
 dataFile = gf$lobeThickness
 AALAtlas = read.csv("/tmp/rminctestdata/AAL.csv")
@@ -14,6 +14,6 @@ reducedVerticesIndices = which(reducedVertices == AALAtlas[atlasIndex,1],arr.ind
 meanThicknessFromVertexFile = mean(gf$nativeRMS_RSLtlink[1,reducedVerticesIndices])
 meanThicknessFromVertexFile
 test_that("Mean Thickness from Vertex File is the same as thickness from Anat File",{
-	expect_that(meanThicknessFromVertexFile,equals(gf$lobeThickness[[1,1]],tolerance = 0.00001))
+	expect_that(meanThicknessFromVertexFile,equals(gf$lobeThickness[[1,1]],tolerance = 0.01))
 })
 
