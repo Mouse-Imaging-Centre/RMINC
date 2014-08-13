38d7986ce516ea88f2d13a795720a5f0053c0c10
Jason Lerch <jason@phenogenomics.ca>
2014-08-13 19:12:13 -0400

    * some documentation updates

--- R/minc_interface.R
+++ R/minc_interface.R
@@ -2181,9 +2181,26 @@ mincLogLikRatio <- function(...) {
   colnames(out) <- outnames
   return(out)
 }
-  
+
+#' computes parametric bootstrap on mincLogLikRatio output
+#'
+#' The Log Likelihood Ratio tests closely approximates a Chi-squared
+#' distribution when the number of groups (i.e. individual subjects in a
+#' longitudinal study) is large (>50), but can be anticonservative when
+#' small. A parametric bootstrap test, in which data is randomly simulated from
+#' the null model and then fit with both models, can give the correct p-value.
+#' Here we compute the parametric boostrap on a small number of randomly chosen
+#' voxels to get a sense of biased the estimated p-values from the log likelihood
+#' ratio test really were.
+#'
+#' @param logLikOutput the output from mincLogLikRatio
+#' @param selection the algorithm for randomly chosing voxels. Only "random" works for now.
+#' @param nsims the number of simulations to run per voxel
+#' @param nvoxels the number of voxels to run the parametric bootstrap on
+#'
+#' @return a matrix containing the chi-square p-values and the bootstrapped p-values
 mincLogLikRatioParametricBootstrap <- function(logLikOutput, selection="random",
-                                               nsims=1000, nvoxels=100) {
+                                               nsims=500, nvoxels=50) {
   mincLmerLists <- attr(logLikOutput, "mincLmerLists")
   mask <- attr(logLikOutput, "mask")
   if (length(mincLmerLists) != 2) {
