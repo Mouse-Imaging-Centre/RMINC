a32256ec1c4dac7d566754246ca28e1993d9b8ae
cfhammill <cfhammill@gmail.com>
2016-08-12 16:56:15 -0400

    request more memory for pbs jobs

--- inst/parallel/pbs_BatchJobs.R
+++ inst/parallel/pbs_BatchJobs.R
@@ -1,7 +1,7 @@
 cluster.functions <- makeClusterFunctionsTorque(system.file("parallel/pbs_script.tmpl", package = "RMINC"))
 default.resources <-
   list(nodes = 1,
-       vmem = "2G",
+       vmem = "8G",
        walltime = "01:00:00",
        modules = NULL,
        module_paths = NULL)
