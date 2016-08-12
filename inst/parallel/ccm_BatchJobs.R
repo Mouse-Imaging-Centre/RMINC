e38bb77d1253ac16ade8319183c969eb32fc0061
cfhammill <cfhammill@gmail.com>
2016-08-12 09:18:00 -0400

    remove stub batchJobs config

--- inst/parallel/ccm_BatchJobs.R
+++
@@ -1,8 +0,0 @@
-cluster.functions <- makeClusterFunctionsTorque(system.file("parallel/pbs_script.tmpl"))
-default.resources <-
-  list(nodes = 1,
-       vmem = "2G",
-       walltime = "01:00:00",
-       modules = NULL,
-       module_paths = NULL,)
-
