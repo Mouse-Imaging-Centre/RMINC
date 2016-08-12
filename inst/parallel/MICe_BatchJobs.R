1ffe73a4743c4a79d8fb1eb7e220062f7fe4998d
cfhammill <cfhammill@gmail.com>
2016-08-12 16:59:46 -0400

    Add MICe configuration

---
+++ inst/parallel/MICe_BatchJobs.R
@@ -0,0 +1,10 @@
+cluster.functions <- makeClusterFunctionsTorque(system.file("parallel/sge_script.tmpl", package = "RMINC"))
+default.resources <-
+  list(nodes = 1,
+       vmem = "8G",
+       walltime = "01:00:00",
+       modules = NULL,
+       module_paths = NULL)
+db.options <- list(pragmas = c("busy_timeout=5000", "journal_mode=WAL"))
+fs.timeout <- 10
+
