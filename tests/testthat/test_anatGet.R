6d08c1bfbbead1a64d90848eaea30dee6765e6eb
cfhammill <cfhammill@gmail.com>
2016-08-28 14:20:34 -0400

    preserve environment across anatGet tests

--- tests/testthat/test_anatGet.R
+++ tests/testthat/test_anatGet.R
@@ -15,69 +15,80 @@ labels <- file.path(dataPath, "test_defs.csv")
 label_frame <- read.csv(labels)
 known_labels <- with(label_frame, union(left.label, right.label))
 
+test_env <- new.env()
+
 test_that("anatGetAll works", {
-  
-  has_mincstuffs <- Sys.which("label_volumes_from_jacobians") != ""
-  skip_if_not(has_mincstuffs)
-  
-  vox_vol <- prod(minc.separation.sizes(segmentation))
-  
-  label_counts <-
-    anatGetAll(segmentation,
-               atlas = segmentation, 
-               method = "labels",
-               defs = labels)
-  
-  label_counts_ref <- 
-    mincGetVolume(segmentation) %>%
-    table %>%
-    `*`(vox_vol) %>%
-    as.data.frame %>%
-    setNames(c("label", "count")) %>%
-    filter(label %in% known_labels)
-  
-  expect_equal(label_counts[1,], label_counts_ref$count, check.attributes = FALSE)
-  
-  label_sums <-
-    anatGetAll(gf$jacobians_0.2, 
-               atlas = segmentation, 
-               method = "sums",
-               defs = labels)
-  
-  seg_vol <- mincGetVolume(segmentation)
-  
-  ref_sums <-
-    sapply(gf$jacobians_0.2, mincGetVolume) %>%
-    apply(2, function(vol) tapply(vol, seg_vol, sum)) %>%
-    t %>%
-    .[,colnames(.) %in% known_labels]
-  
-  expect_equal(unclass(label_sums), ref_sums, tolerance = 10e-5, check.attributes = FALSE)
-  
-  label_means <-
-    anatGetAll(gf$jacobians_0.2, 
-               atlas = segmentation, 
-               method = "means",
-               defs = labels)
-  
-  expect_equal(sweep(label_sums, 2, label_counts / vox_vol, FUN = `/`), 
-               label_means, tol = 10e-5, ignore.attributes = TRUE)
-  #curious to note computing counts then sums and sweeping dividing
-  #out the volume is faster than anatGetAll with means...
-  
-  jacobians <-
-    anatGetAll(gf$jacobians_0.2, 
-               atlas = segmentation, 
-               method = "jacobians",
-               defs = labels)
-  
-  ref_jacobians <-
-    sapply(gf$jacobians_0.2, mincGetVolume) %>%
-    apply(2, function(vol) tapply(vol, seg_vol, function(j) sum(exp(j) * vox_vol))) %>%
-    t %>%
-    .[,colnames(.) %in% known_labels]
-  
-  expect_equal(ref_jacobians, 
-               unclass(jacobians), tol = 10e-5, check.attributes = FALSE)
+  evalq({
+    has_mincstuffs <- as.character(Sys.which("label_volumes_from_jacobians")) != ""
+    skip_if_not(has_mincstuffs)
+    
+    vox_vol <- prod(minc.separation.sizes(segmentation))
+    
+    label_counts <-
+      anatGetAll(segmentation,
+                 atlas = segmentation, 
+                 method = "labels",
+                 defs = labels)
+    
+    label_counts_ref <- 
+      mincGetVolume(segmentation) %>%
+      table %>%
+      `*`(vox_vol) %>%
+      as.data.frame %>%
+      setNames(c("label", "count")) %>%
+      filter(label %in% known_labels)
+    
+    expect_equal(label_counts[1,], label_counts_ref$count, check.attributes = FALSE)
+    
+    label_sums <-
+      anatGetAll(gf$jacobians_0.2, 
+                 atlas = segmentation, 
+                 method = "sums",
+                 defs = labels)
+    
+    seg_vol <- mincGetVolume(segmentation)
+    
+    ref_sums <-
+      sapply(gf$jacobians_0.2, mincGetVolume) %>%
+      apply(2, function(vol) tapply(vol, seg_vol, sum)) %>%
+      t %>%
+      .[,colnames(.) %in% known_labels]
+    
+    expect_equal(unclass(label_sums), ref_sums, tolerance = 10e-5, check.attributes = FALSE)
+    
+    label_means <-
+      anatGetAll(gf$jacobians_0.2, 
+                 atlas = segmentation, 
+                 method = "means",
+                 defs = labels)
+    
+    expect_equal(sweep(label_sums, 2, label_counts / vox_vol, FUN = `/`), 
+                 label_means, tol = 10e-5, ignore.attributes = TRUE)
+    #curious to note computing counts then sums and sweeping dividing
+    #out the volume is faster than anatGetAll with means...
+    
+    jacobians <-
+      anatGetAll(gf$jacobians_0.2, 
+                 atlas = segmentation, 
+                 method = "jacobians",
+                 defs = labels)
+    
+    ref_jacobians <-
+      sapply(gf$jacobians_0.2, mincGetVolume) %>%
+      apply(2, function(vol) tapply(vol, seg_vol, function(j) sum(exp(j) * vox_vol))) %>%
+      t %>%
+      .[,colnames(.) %in% known_labels]
+    
+    expect_equal(ref_jacobians, 
+                 unclass(jacobians), tol = 10e-5, check.attributes = FALSE)
+  }, envir = test_env)
 })
+
+test_that("AnatGetFile2 works", {
+  evalq({
+    new_jacobians <- anatGetAll2(gf$jacobians_0.2, atlas = segmentation, method = "jacobians")
+  }, envir = test_env)
+})
+
+
 
