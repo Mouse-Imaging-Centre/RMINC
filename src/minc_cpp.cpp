ba6d708f9e4c98cd95348cc1349ac4475bba6d09
cfhammill <cfhammill@gmail.com>
2016-10-25 17:09:17 -0400

    Add path->filename code
    deals with the problem of files sharing
    a filename and differing only by path.
    Finds the longest common subpath, trims that
    from each path, then converts slashes to
    underscores. Voila, unique names, provided
    the file does not occur twice...

--- src/minc_cpp.cpp
+++ src/minc_cpp.cpp
@@ -1,8 +1,18 @@
 #include <Rcpp.h>
 #include "minc2.h"
+#include <stack>
 #include "minc_cpp.h"
 using namespace Rcpp;
 using namespace std;
+
+template <typename T>
+string NumberToString ( T Number )
+{
+  ostringstream ss;
+  ss.clear();
+  ss << Number;
+  return ss.str();
+}
 
 void cautious_get_hyperslab(mihandle_t volume,
                             mitype_t buffer_data_type,
@@ -119,4 +129,58 @@ vector<misize_t> get_volume_dimensions(mihandle_t volume){
   }
   
   return(volume_dimensions);
+}
+
+CharacterVector path_to_filename(CharacterVector filenames){
+  Environment base("package:base");
+  Function strsplit = base["strsplit"];
+  Function paste = base["paste"];
+  
+  List split_res(filenames.size());
+  CharacterVector lasts(filenames.size());
+  NumericVector lengths(filenames.size());
+  for(int i = 0; i < filenames.size(); ++i){
+    CharacterVector res = as<CharacterVector>(as<List>(strsplit(as<std::string>(filenames[i]), "/"))[0]);
+
+    for(int r = 0; r < res.size(); ++r) //Remove empty substrings
+      if(res[r] == "") res.erase(res.begin() + r, res.begin() + r + 1);
+    
+    split_res[i] = res;
+    lengths[i] = res.size();
+    lasts[i] = res[lengths[i] - 1];
+  }
+  
+  // Check if there are any duplicates in last
+  if(unique(lasts).size() == lasts.size())
+    return(lasts);
+  
+  int longest_possible_sub = (int) min(lengths) - 1;
+  bool all_equal = true;
+  for(int i = 0; i < longest_possible_sub; ++i){
+    for(int f = 1; f < filenames.size(); ++f){
+      if(as<CharacterVector>(split_res[0])[0] != as<CharacterVector>(split_res[f])[0]){
+        all_equal = false;
+        break;
+      }
+    }
+    
+    // if they\'re all the same trim them
+    if(all_equal){
+      for(int f = 0; f < filenames.size(); ++f){
+        CharacterVector res = split_res[f];
+        CharacterVector::iterator it = res.begin();
+        res.erase(it, it + 1);
+        split_res[f] = res;
+      }
+    }
+  }
+  
+  CharacterVector results(filenames.size());
+  for(int f = 0; f < filenames.size(); ++f){
+    results[f] = as<CharacterVector>(paste(split_res[f], Named("collapse", "_")))[0];
+  }
+  
+  return(results); 
+}
+
 }
