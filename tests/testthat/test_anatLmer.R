b66c3823f32e92cf40c93fd014c8fe1089fcf163
cfhammill <cfhammill@gmail.com>
2016-11-02 16:14:16 -0400

    Test exotic formulae in anatLmer

--- tests/testthat/test_anatLmer.R
+++ tests/testthat/test_anatLmer.R
@@ -16,35 +16,63 @@ labels <- file.path(dataPath, "test_defs.csv")
 label_frame <- read.csv(labels)
 known_labels <- with(label_frame, union(left.label, right.label))
 
+anat_env <- new.env()
+
 test_that("anatLmer works", {
-  
-  has_mincstuffs <- as.character(Sys.which("label_volumes_from_jacobians")) != ""
-  skip_if_not(has_mincstuffs)
-  
-  jacobians <-
-    anatGetAll(gf$jacobians_0.2, 
-               atlas = segmentation, 
-               method = "jacobians",
-               defs = labels)
-  
-  suppressWarnings(
-    lmer_res_nlhs <- anatLmer( ~ Pain.sensitivity + (1|Genotype), 
+  evalq({
+    has_mincstuffs <- as.character(Sys.which("label_volumes_from_jacobians")) != ""
+    skip_if_not(has_mincstuffs)
+    
+    jacobians <-
+      anatGetAll(gf$jacobians_0.2, 
+                 atlas = segmentation, 
+                 method = "jacobians",
+                 defs = labels)
+    
+    suppressWarnings(
+      lmer_res_nlhs <- anatLmer( ~ Pain.sensitivity + (1|Genotype), 
+                                 data = gf, anat = jacobians))
+    
+    suppressWarnings(
+      lmer_res_lhs <- anatLmer(dummy ~ Pain.sensitivity + (1|Genotype), 
                                data = gf, anat = jacobians))
-  
-  suppressWarnings(
-    lmer_res_lhs <- anatLmer(dummy ~ Pain.sensitivity + (1|Genotype), 
-                             data = gf, anat = jacobians))
-  
-  suppressWarnings({
-    lmer_ref <-
-      apply(jacobians, 2, function(col){
-        gf2 <- gf %>% mutate(resp = col)
-        mod <- lmer(resp ~ Pain.sensitivity + (1|Genotype), data = gf2)
-        RMINC:::mincLmerExtractVariables(mod)
-      }) %>%
-      t
+    
+    suppressWarnings({
+      lmer_ref <-
+        apply(jacobians, 2, function(col){
+          gf2 <- gf %>% mutate(resp = col)
+          mod <- lmer(resp ~ Pain.sensitivity + (1|Genotype), data = gf2)
+          RMINC:::mincLmerExtractVariables(mod)
+        }) %>%
+        t
+    })
+    
+    expect_equal(unclass(lmer_res_nlhs), lmer_ref, tolerance = 10e-5, check.attributes = FALSE)
+    expect_equal(lmer_res_lhs, lmer_res_nlhs, tolerance = 10e-5, check.attributes = FALSE)
+  }, envir = anat_env)
+})
+
+test_that("anatLmer estimate DF returns sensible results", {
+  evalq({
+    expect_warning(verboseRun(with_dfs <- anatLmerEstimateDF(lmer_res_nlhs)),
+                   regex = "Unable to estimate")
+    dfs <- attr(with_dfs, "df")
+    expect_true(between(dfs[1], 1, 3))
+    expect_true(all(between(dfs[2:4], 30, 50)))            
+  }, envir = anat_env)
+})
+
+test_that("anatLmer exotic formulae work", {
+  evalq({
+    verboseRun(
+      exotic_lmer <- anatLmer(~ I(factor(as.numeric(Pain.sensitivity) - 1)) + (1 | Genotype)
+                              , data = gf, anat = jacobians)
+    )
+    
+    expect_warning(verboseRun(with_dfs <- anatLmerEstimateDF(exotic_lmer)),
+                   regex = "Unable to estimate")
+    dfs <- attr(with_dfs, "df")
+    expect_true(between(dfs[1], 1, 3))
+    expect_true(all(between(dfs[2:4], 30, 50)))
   })
-  
-  expect_equal(unclass(lmer_res_nlhs), lmer_ref, tolerance = 10e-5, check.attributes = FALSE)
-  expect_equal(lmer_res_lhs, lmer_res_nlhs, tolerance = 10e-5, check.attributes = FALSE)
 })
