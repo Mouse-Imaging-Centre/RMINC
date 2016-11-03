167c384ef9f4a1b43e0c9568253d690039ce0470
cfhammill <cfhammill@gmail.com>
2016-11-03 16:55:58 -0400

    Add mincstuffs skips

--- tests/testthat/test_anatLmer.R
+++ tests/testthat/test_anatLmer.R
@@ -54,6 +54,8 @@ test_that("anatLmer works", {
 
 test_that("anatLmer estimate DF returns sensible results", {
   evalq({
+    skip_if_not(has_mincstuffs)
+    
     expect_warning(verboseRun(with_dfs <- anatLmerEstimateDF(lmer_res_nlhs)),
                    regex = "Unable to estimate")
     dfs <- attr(with_dfs, "df")
@@ -64,6 +66,8 @@ test_that("anatLmer estimate DF returns sensible results", {
 
 test_that("anatLmer exotic formulae work", {
   evalq({
+    skip_if_not(has_mincstuffs)
+    
     verboseRun(
       exotic_lmer <- anatLmer(~ I(factor(as.numeric(Pain.sensitivity) - 1)) + (1 | Genotype)
                               , data = gf, anat = jacobians)
