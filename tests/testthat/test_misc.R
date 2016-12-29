00498f340d5b3c349247de2e9d2320b122f12114
cfhammill <cfhammill@gmail.com>
2016-12-29 16:17:35 -0500

    Check file checks do something

--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -7,3 +7,18 @@ test_that("File limit check works", {
   expect_true(RMINC:::enoughAvailableFileDescriptors(0))
   expect_false(RMINC:::enoughAvailableFileDescriptors(Inf, error = FALSE))
 })
+
+test_that("File checking code all fails", {
+  cur_ulim <- RMINC:::checkCurrentUlimit()
+  skip_if_not(is.finite(cur_ulim))
+  
+  frame <- 
+    data_frame(
+      files = rep("", cur_ulim + 1)
+      , group = c("a", "b"))
+  
+  expect_error(mincLmer(files ~ (1 | group), data = frame))
+  expect_error(mincMean(frame$files))
+  expect_error(anatGetAll(frame$files, method = "labels"))
+  expect_error(vertexLm(files ~ group, data = frame))
+})
