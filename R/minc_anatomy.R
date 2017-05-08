90ee59dcf0a9ad608364376bee02fc17d1f783be
cfhammill <cfhammill@gmail.com>
2017-05-08 13:19:21 -0400

    Export new anatSummarize

--- R/minc_anatomy.R
+++ R/minc_anatomy.R
@@ -526,6 +526,22 @@ label_counts <-
     results
   }
 
+#' Summarize anatGetAll results by hierarchy
+#' 
+#' Take the results of \link{anatGetAll} and summarize a grouping label, typically the structures
+#' group in the anatomical hierarchy. 
+#' 
+#' @param anat The results of a call to \link{anatGetAll}
+#' @param summarize_by either a data frame with grouping information or a path to label definitions. 
+#' If a data frame is passed it must have two columns: "label" containing the structure
+#' name (see \code{colnames(anat)} to check the relevant labels) and "grouping" containg the name 
+#' of the group each structure belongs to.
+#' @param discard_missing logical controlling how to handle structures with no ("") group information.
+#' If TRUE filter these structures, if FALSE give each structure a group label to match their structure
+#' name.
+#' @return A matrix with columns representing groups from the hierarchy and rows representing individuals
+#' with values equal to the sum of the individual members of each group.
+#' @export 
 anatSummarize <-
   function(anat
            , summarize_by = "hierarchy"
