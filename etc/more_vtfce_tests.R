2cad4b574f582fabce7f2b796643458d545e8594
cfhammill <cfhammill@gmail.com>
2017-05-23 11:17:10 -0400

    vtfce experiments update

--- etc/more_vtfce_tests.R
+++ etc/more_vtfce_tests.R
@@ -17,6 +17,12 @@ system.time(wtfce <- vertexTFCE(mincGetVolume("../../../POND/asymmetry201701101/
                                 , side = "positive"
                                 , weight = .001))
 
+system.time(btfce <- vertexTFCE(mincGetVolume("../../../POND/asymmetry201701101/stats/test_t_downsamp.mnc")
+                                , igraph::graph.adjlist(lapply(adj, `+`, 1))
+                                , E = .5, H = 2, nsteps = 100
+                                , side = "both"
+                                , weight = .001))
+
 new_adj_list1 <- function(graph, mode = c("all", "in", "out", "total"))
 {
   if (!igraph::is_igraph(graph)) {
