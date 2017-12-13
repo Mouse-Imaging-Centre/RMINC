0a4ff59c56b24b86e71b726724f9a6693b3098c7
cfhammill <cfhammill@gmail.com>
2017-12-13 13:49:51 -0500

    Substantial rewrite of qminc tests
    The old version didn't work, when trying
    to run slurm/sge jobs it didn't have access
    to the test data. So I rewrote it to
    download the data to the local directory
    if necessary. I encapsulated it all
    in a function to use `on.exit`

--- tests/testthat/test_qminc.R
+++ tests/testthat/test_qminc.R
@@ -2,83 +2,94 @@ library(testthat)
 library(batchtools)
 context("Parallel Functions")
 
-getRMINCTestData()
-dataPath <- file.path(tempdir(), "rminctestdata/")
- 
-gf <- read.csv(file.path(dataPath, "test_data_set.csv"))
-mask_file <- file.path(dataPath, "testminc-mask.mnc")
+## Write all tests as a function
+## Skipping data download if on cran or travis
+## downloading a dir local copy of RMINC test data
+## so that queue jobs can be tested.
+## not my favourite code ever.
+test_parallel <-
+  function(){
 
-test_that("Test sequential, multicore, and queue applies work", {
-  
-  skip_on_cran()
-  skip_on_travis()
-  
-  if(Sys.getenv("TEST_Q_MINC") != "yes") 
-    skip("qMinc tests disabled")
-  
-  options(BBmisc.ProgressBar.style = "off")
+    old_options <- options()
+    options(batchtools.verbose = getOption("verbose"))
+    on.exit({ options(old_options) })
     
-  m_sequential <- verboseRun(
-    mincApplyRCPP(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10)),
-    getOption("verbose"))
-  
-  m_queue <- verboseRun(
-    qMincApply(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10)),
-    getOption("verbose")
-  )
-  
-  m_multicore <- verboseRun(
-    mcMincApply(gf$jacobians_fixed_2, mean, slab_sizes = c(10,10,10), cores = min(2, parallel::detectCores() - 1)),
-    getOption("verbose")
-  )
-  
-  expect_equivalent(m_sequential, m_queue)
-  expect_equivalent(m_sequential, m_multicore)
-  expect_equal(attr(m_sequential, "filenames"), attr(m_queue, "filenames"))
-  expect_equal(attr(m_sequential, "filenames"), attr(m_multicore, "filenames"))
-  expect_equal(attr(m_sequential, "likeVolume"), attr(m_queue, "likeVolume"))
-  expect_equal(attr(m_sequential, "likeVolume"), attr(m_multicore, "likeVolume"))
-})
+    if(identical(Sys.getenv("NOT_CRAN"), "true")
+       && !identical(Sys.getenv("TRAVIS"), "true")){
 
-test_that("Masking qMincApply behaves as expected", {
-  
-  skip_on_cran()
-  skip_on_travis()
-  
-  if(Sys.getenv("TEST_Q_MINC") != "yes") 
-    skip("qMinc tests disabled")
-  
-  m_sequential <- verboseRun(
-    mincApplyRCPP(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10), mask = mask_file),
-    getOption("verbose"))
-  
-  m_queue <- verboseRun(
-    qMincApply(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10), mask = mask_file),
-    getOption("verbose")
-  )
-  
-  expect_equivalent(m_sequential, m_queue)
-})
+      if(!file.exists("rminctestdata")){
+        
+        if(getOption("verbose")){
+          getRMINCTestData("./")
+        } else {
+          capture.output(getRMINCTestData("./"), type = "message")
+        }
+        
+        on.exit({
+          unlink("rminctestdata", recursive = TRUE)
+          unlink("rminctestdata.tar.gz")
+        }, add = TRUE)
+        
+      }
+      
+      dataPath <- file.path("rminctestdata/")
+      
+      gf <- read.csv(file.path(dataPath, "test_data_set.csv"))
+      mask_file <- file.path(dataPath, "testminc-mask.mnc")          
+    }
 
-setConfig(old_config)
-options(old_options)
+    test_that("Test sequential, multicore, and queue applies work", {
+      
+      skip_on_cran()
+      skip_on_travis()
+      
+      if(Sys.getenv("TEST_Q_MINC") != "yes") 
+        skip("qMinc tests disabled")
+        
+        m_sequential <- verboseRun(
+          mincApplyRCPP(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10)),
+          getOption("verbose"))
+        
+        m_queue <- verboseRun(
+          qMincApply(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10)),
+          getOption("verbose")
+        )
+        
+        m_multicore <- verboseRun(
+          mcMincApply(gf$jacobians_fixed_2, mean, slab_sizes = c(10,10,10)
+                    , cores = min(2, parallel::detectCores() - 1)),
+          getOption("verbose")
+        )
+        
+        expect_equivalent(m_sequential, m_queue)
+        expect_equivalent(m_sequential, m_multicore)
+        expect_equal(attr(m_sequential, "filenames"), attr(m_queue, "filenames"))
+        expect_equal(attr(m_sequential, "filenames"), attr(m_multicore, "filenames"))
+        expect_equal(attr(m_sequential, "likeVolume"), attr(m_queue, "likeVolume"))
+        expect_equal(attr(m_sequential, "likeVolume"), attr(m_multicore, "likeVolume"))
+    })
 
-# if(getOption("RMINC_QUEUE") == "sge"){
-#   gf2 <- gf
-#   gf2$jacobians_fixed_2 <- sub("/tmp/", "/hpf/largeprojects/MICe/chammill/", gf$jacobians_fixed_2)
-# 
-#   ms_2 <-  verboseRun(
-#     "mincApplyRCPP(gf2$jacobians_fixed_2, mean, slab_sizes = c(5,1,10))",
-#     getOption("verbose"))
-# 
-#   mp_sge <- verboseRun("qMincApply(gf2$jacobians_fixed_2, mean, parallel_method = 'sge',
-#                        slab_sizes = c(5,1,10), temp_dir = normalizePath(\"~/sge_reg\"))",
-#                        getOption("verbose"))
-# 
-#   test_that("SGE parallel matches sequential", {
-#     expect_equivalent(mp_sge, ms_2)
-#     expect_equal(attr(mp_sge, "filenames"), attr(ms_2, "filenames"))
-#     expect_equal(attr(mp_sge, "likeVolume"), attr(ms_2, "likeVolume"))
-#   })
-# }
+    test_that("Masking qMincApply behaves as expected", {
+      
+      skip_on_cran()
+      skip_on_travis()
+      
+      if(Sys.getenv("TEST_Q_MINC") != "yes") 
+        skip("qMinc tests disabled")
+        
+        m_sequential <- verboseRun(
+          mincApplyRCPP(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10), mask = mask_file),
+          getOption("verbose"))
+        
+        m_queue <- verboseRun(
+          qMincApply(gf$jacobians_fixed_2, mean, slab_sizes = c(5,1,10), mask = mask_file),
+          getOption("verbose")
+        )
+        
+        expect_equivalent(m_sequential, m_queue)
+    })
+  }
 
+################################
+## Run the tests
+test_parallel()
