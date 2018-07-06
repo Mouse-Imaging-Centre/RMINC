a576ba13c0c55eafa2f6e237f7820812fed4ac26
cfhammill <cfhammill@gmail.com>
2018-07-06 15:48:27 -0400

    Add purrr to my paccckage

--- R/RMINC.R
+++ R/RMINC.R
@@ -32,6 +32,7 @@
 #' @importFrom visNetwork visNetwork visNodes visEdges visHierarchicalLayout
 #'                        visPhysics
 #' @importFrom rjson fromJSON
+#' @importFrom purrr map map_df
 #' @useDynLib RMINC, .registration = TRUE 
 #' @docType package
 #' @name RMINC
