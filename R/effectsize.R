9b9fef13bd8a63f15206f91282168ad805e69639
Gabriel A. Devenyi <gdevenyi@gmail.com>
2018-10-29 10:52:58 -0400

    Fix detection of reference group for mutilevel factors, adjust docs

--- R/effectsize.R
+++ R/effectsize.R
@@ -3,7 +3,7 @@
 #' Takes the output of a minc modelling function and computes the unbiased
 #' hedges g* and variance of hedges g*
 #' @param buffer The results of a vertex/anat/mincLm run
-#' @param columns A vector of factor predictor names. By default the threshold will
+#' @param predictors A vector of factor predictor names. By default the effect size
 #' be computed for all factor columns.
 #' @details This code implements the methods from Nakagawa, S., Cuthill, I.C., 2007. Effect size, confidence interval and statistical significance: a practical guide for biologists. Biol. Rev. Camb. Philos. Soc. 82, 591–605. https://doi.org/10.1111/j.1469-185X.2007.00027.x
 #' for computing effect size of group comparisons from a GLM.
@@ -19,7 +19,7 @@
 #' effectsize <- mincEffectSize(vs)
 #' }
 #' @export
-vertexEffectSize <- function(buffer, columns = NULL)
+vertexEffectSize <- function(buffer, predictors = NULL)
 {
   #Nakagawa, S., Cuthill, I.C., 2007. Effect size, confidence interval and statistical significance: a practical guide for biologists. Biol. Rev. Camb. Philos. Soc. 82, 591–605. https://doi.org/10.1111/j.1469-185X.2007.00027.x
   #Original unbiased corrector from paper replaced with
@@ -40,9 +40,11 @@ vertexEffectSize <- function(buffer, columns = NULL)
   # g_biased = t ( n1 + n2 ) / sqrt(n1*n2)*sqrt(df)
   # g_unbiased = g_biased * J(n1 + n2 - 2)
   # g_variance_unbiased = (n1 + n2) / (n1*n2) + g_unbiased**2 / (2 * (n1 + n2 - 2))
+  # n1 study group
+  # n2 reference group
   # N = n1+n2
   # NN = n1*n2
-  
+
   originalMincAttrs <- mincAttributes(buffer)
   stattype <- originalMincAttrs$`stat-type`
   # Remove coefficients from buffer
@@ -65,32 +67,50 @@ vertexEffectSize <- function(buffer, columns = NULL)
         updatedAttrs$`stat-type`[-indicesToRemove]
       updatedAttrs$dimnames[[2]] <-
         updatedAttrs$dimnames[[2]][-indicesToRemove]
-      
+
       buffer <- buffer[, -indicesToRemove]
       buffer <- setMincAttributes(buffer, updatedAttrs)
     }
   }
-  
+
   model_call <- attr(buffer, "call")
   model_call[[1]] <- quote(model.matrix)
   names(model_call)[names(model_call) == "formula"] <- ""
   mod_mat <- eval(model_call)
   conts <- attr(mod_mat, "contrasts")
+  cat_vars <-
+    names(conts)[vapply(conts, function(x)
+      all(x == "contr.treatment"), logical(1))]
   if (is.null(conts))
     stop("No categorical variables in model, cannot compute g* statistics")
-  if (!all(sapply(columns, function(x) any(grepl(x, updatedAttrs$dimnames[[2]])))))
-    stop("Columns not found in model")
+  if (!all(sapply(predictors, function(x)
+    any(grepl(
+      x, updatedAttrs$dimnames[[2]]
+    )))))
+    stop("Supplied predictors not found in model")
+  if (!all(conts == "contr.treatment"))
+    stop(
+      "Non-treatment factors present in model, effect size is not meaningful
+      for this configuration"
+    )
 
-  if (is.null(columns)) {
-    cat_vars <-
-      names(conts)[vapply(conts, function(x)
-        all(x == "contr.treatment"), logical(1))]
-    columns <- lapply(updatedAttrs$dimnames, function(x)
+  if (is.null(predictors)) {
+    predictors <- lapply(updatedAttrs$dimnames, function(x)
       grep(paste(cat_vars, collapse = "|"), x, value = TRUE))[[2]]
-    columns <- gsub("tvalue-", "", columns, fixed = TRUE)
+    predictors <- gsub("tvalue-", "", predictors, fixed = TRUE)
   }
-  
-  n.cols <- length(columns)
+
+  referencegroupsize <-
+    matrix(1, nrow = 1, ncol = length(predictors))
+  colnames(referencegroupsize) <- predictors
+
+  for (var in cat_vars) {
+    referencegroupsize[,  grep(var, predictors, value = TRUE)] <-
+      NROW(updatedAttrs$model[, grep(var, colnames(updatedAttrs$model))]) -
+      sum(updatedAttrs$model[, grep(var, colnames(updatedAttrs$model))])
+  }
+
+  n.cols <- length(predictors)
   n.row <- 0
   if (is.matrix(buffer)) {
     n.row <- nrow(buffer)
@@ -100,12 +120,12 @@ vertexEffectSize <- function(buffer, columns = NULL)
   }
   output <- matrix(1, nrow = n.row, ncol = 2 * n.cols)
   colnames(output) <-
-    c(paste0("hedgesg-", columns),
-      paste0("hedgesg_var-", columns))
-  for (column in columns) {
+    c(paste0("hedgesg-", predictors),
+      paste0("hedgesg_var-", predictors))
+  for (column in predictors) {
     n1 <- sum(updatedAttrs$model[, column])
-    N <- length(updatedAttrs$model[, column])
-    n2 <- N - n1
+    n2 <- referencegroupsize[, column]
+    N <- n1 + n2
     NN <- n1 * n2
     df <- updatedAttrs$df[[1]][2]
     output[, paste0("hedgesg-", column)] <-
@@ -116,16 +136,16 @@ vertexEffectSize <- function(buffer, columns = NULL)
   }
   rownames(output) <- rownames(buffer)
   attr(output, "likeVolume") <- attr(buffer, "likeVolume")
-  
+
   # run the garbage collector...
   gcout <- gc()
-  
+
   return(output)
 }
 
 #' @describeIn vertexEffectSize mincEffectSize
 #' @export
-mincEffectSize <- function(buffer, columns = NULL) {
+mincEffectSize <- function(buffer, predictors = NULL) {
   eff_call <- match.call()
   eff_call[[1]] <- quote(vertexEffectSize)
   eval(eff_call)
@@ -133,8 +153,8 @@ mincEffectSize <- function(buffer, columns = NULL) {
 
 #' @describeIn vertexEffectSize anatEffectSize
 #' @export
-anatEffectSize <- function(buffer, columns = NULL) {
+anatEffectSize <- function(buffer, predictors = NULL) {
   eff_call <- match.call()
   eff_call[[1]] <- quote(vertexEffectSize)
   eval(eff_call)
-} 
+}
