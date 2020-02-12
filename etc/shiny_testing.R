210f3cc167c20741209055d983ee51a878bb7c1a
cfhammill <cfhammill@gmail.com>
2020-02-12 17:27:57 -0500

    Fix broken FDR selector and spurious % in shinyRMINC
    Fixes #270

---
+++ etc/shiny_testing.R
@@ -0,0 +1,16 @@
+#/hpf/largeprojects/MICe/matthijs/2016-06-Boughner-embryos/2017-03-TFCE
+
+library(RMINC)
+
+gfE14.0 <- read.csv("/hpf/largeprojects/MICe/matthijs/2016-06-Boughner-embryos/Boughner-to-E14.0_analysis/filename_mapping.csv")
+gfE14.0_no_outlier <- gfE14.0[-c(8),]
+gfE14.0_no_outlier$abs_jac_fwhm0.2 <- sub(".mnc", "_fwhm0.2.mnc", gfE14.0_no_outlier$abs_jac_no_blur)
+gfE14.0_no_outlier$rel_jac_fwhm0.2 <- sub("_with_additional_inverted_absolute", "_inverted_pure_nlin_relative", gfE14.0_no_outlier$abs_jac_fwhm0.2)
+
+vs <- mincLm(rel_jac_fwhm0.2 ~ genotype, gfE14.0_no_outlier
+           , mask="/hpf/largeprojects/MICe/matthijs/2016-06-Boughner-embryos/Boughner-to-E14.0/minctracc_nlin/minctracc-nlin-6_rough_mask_fix_zyx.mnc")
+
+vs_fdr <- mincFDR(vs)
+ex <- mincGetVolume(gfE14.0$abs_jac_no_blur[1])
+options("shiny.port" = 8000)
+launch_shinyRMINC(vs, mincArray(ex), fdr = vs_fdr)
