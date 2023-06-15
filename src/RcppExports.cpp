2d08d6e9b3f89761ccd3c9a050143041668adb72
Yohan Yee <yohan.yee@gmail.com>
2023-06-15 02:26:28 -0400

    Addresses the chaos caused by hidden length arguments in R>=4.3 (R now adds hidden length arguments when calling BLAS / Lapack functions from C, see https://blog.r-project.org/2019/09/25/gfortran-issues-with-lapack-ii/). This is fixed using the solution described in the Writing R Extensions manual (https://rstudio.github.io/r-manuals/r-exts/The-R-API.html#fortran-character-strings), i.e. invoking the FCONE macro.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // anat_summary
 List anat_summary(CharacterVector filenames, IntegerVector atlas, std::string method);
 RcppExport SEXP _RMINC_anat_summary(SEXP filenamesSEXP, SEXP atlasSEXP, SEXP methodSEXP) {
